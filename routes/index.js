3ad6f72fa131b471192dd1ba3cecd169c175af20
tUrG0n <yordanov.piotr@gmail.com>
2012-08-05 22:08:17 +0300

    Working music portfolio

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
  */
 
 exports.music = function(req, res){
-  res.render('main', { music:'Music', title: 'Piotr Yordanov' })
+  res.render('music', { music:'', title: 'Piotr Yordanov' })
 };
 exports.index = function(req, res){
   res.render('main', { music:'', title: 'Piotr Yordanov' })
