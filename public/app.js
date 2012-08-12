fde258229754dccc1c648d99444a7514a0a574fb
tUrG0n <yordanov.piotr@gmail.com>
2012-08-12 03:52:06 +0300

    Added twitter support and fixed view routing

--- public/app.js
+++ public/app.js
@@ -1,23 +1,63 @@
-$(window).load(function(){
-	$(function(){
-		$('.container').masonry({
-			itemSelector: '.youtube'
-		});
+function tweet(){
+	$('#tweets').jTweetsAnywhere({
+			username: 'yordaKhof',
+			count: 5,
+			showTweetFeed: {
+					showProfileImages: true,
+					showUserScreenNames: true,
+					showUserFullNames: true,
+					showActionReply: true,
+					showActionRetweet: true,
+					showActionFavorite: true
+			},
+			tweetTimestampDecorator: function(tweet, options){
+				var screenName = tweet.user ? tweet.user.screen_name : false || tweet.from_user;
+				var date = new Date(formatDate(tweet.created_at));
+
+				var dateString =
+					date.getHours() + ':' +
+					(date.getMinutes() < 10 ? '0' : '') +
+					date.getMinutes() + ' ' +
+					(date.getMonth() + 1) + '/' +
+					date.getDate() + '/' +
+					date.getFullYear();
+
+				var html =
+					'<span class="jta-tweet-timestamp"> "' +
+					'<a class="jta-tweet-timestamp-link" href="http://twitter.com/' +
+					screenName + '/status/' + tweet.id + '">' +
+					dateString + '</a></span>';
+
+				return html;
+			}
 	});
+}
+$(window).load(function(){
 	$(function () {
 		App = Em.Application.create();
 
 		App.ApplicationController = Em.Controller.extend();
 		App.ApplicationView = Em.View.extend({ templateName: 'application' });
 
-		App.NavbarController = Em.Controller.extend();
-		App.NavbarView = Em.View.extend({ templateName: 'navbar' });
-
 		App.HomeController = Em.Controller.extend();
-		App.HomeView = Em.View.extend({ templateName: 'home' });
+		App.HomeView = Em.View.extend({
+			templateName: 'home',
+			didInsertElement: function() {
+				tweet();
+			}
+		});
 
-		App.StartingMenuController = Em.Controller.extend();
-		App.StartingMenuView = Em.View.extend({ templateName: 'getting-started-menu' });
+		App.AboutController = Em.Controller.extend();
+		App.AboutView = Em.View.extend({ templateName: 'about' });
+
+		App.PortfolioController = Em.Controller.extend();
+		App.PortfolioView = Em.View.extend({ templateName: 'portfolio' });
+
+		App.MusicController = Em.Controller.extend();
+		App.MusicView = Em.View.extend({ templateName: 'Music' });
+
+		App.MusicMenuController = Em.Controller.extend();
+		App.MusicMenuView = Em.View.extend({ templateName: 'music-menu' });
 
 		App.YoutubeController = Em.Controller.extend();
 		App.YoutubeView = Em.View.extend({ templateName: 'youtube' });
@@ -25,18 +65,21 @@ $(window).load(function(){
 		App.SoundCloudController = Em.Controller.extend();
 		App.SoundCloudView = Em.View.extend({ templateName: 'soundCloud' });
 
-		App.MusicController = Em.Controller.extend();
-		App.MusicView = Em.View.extend({ templateName: 'Music' });
-
 		App.Router = Em.Router.extend({
 			enableLogging: true,
 			location: 'hash',
 
 			root: Em.Route.extend({
-				// EVENTS
 				gotoHome: Ember.Route.transitionTo('home'),
-				gotoMusic: Ember.Route.transitionTo('Music.soundCloud'),
+				gotoMusic: Ember.Route.transitionTo('music'),
+				gotoPortfolio: Ember.Route.transitionTo('portfolio'),
+				gotoAbout: Ember.Route.transitionTo('about'),
 
+				// STATES
+        index: Ember.Route.extend({
+                route: '/',
+                redirectsTo: 'home'
+            }),
 				// STATES
 				home: Em.Route.extend({
 					route: '/',
@@ -44,16 +87,34 @@ $(window).load(function(){
 						router.get('applicationController').connectOutlet('home');
 					}
 				}),
-				Music: Em.Route.extend({
+				portfolio: Em.Route.extend({
+					route: '/portfolio',
+					connectOutlets: function (router, context) {
+						router.get('applicationController').connectOutlet('portfolio');
+					}
+				}),
+				about: Em.Route.extend({
+					route: '/about',
+					connectOutlets: function (router, context) {
+						router.get('applicationController').connectOutlet('about');
+					}
+				}),
+				music: Em.Route.extend({
 					// SETUP
 					route: '/Music',
 					connectOutlets: function (router, context) {
-						router.get('applicationController').connectOutlet('Music');
+						router.get('applicationController').connectOutlet('music');
 					},
 					// EVENTS
 					gotoSoundCloud: Ember.Route.transitionTo('soundCloud'),
 					gotoYoutube: Ember.Route.transitionTo('youtube'),
 
+					music: Em.Route.extend({
+							route: '/music',
+							connectOutlets: function (router, context) {
+									router.get('applicationController').connectOutlet('Music');
+							} 
+					}),
 					soundCloud: Em.Route.extend({
 							route: '/soundCloud',
 							connectOutlets: function (router, context) {
